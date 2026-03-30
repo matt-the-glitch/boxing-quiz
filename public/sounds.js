7ad7fb67b18b2a3a7e175693da71aea6949e8e7e
Matthias Milleker <k12@Macbook-Pro-von-Matthias.fritz.box>
2026-03-30 19:50:48 +0200

    Fix audio on iOS: unlock sounds on first user gesture

--- public/sounds.js
+++ public/sounds.js
@@ -4,10 +4,18 @@
   const cheer = new Audio("sounds/cheer.mp3");
   const boo   = new Audio("sounds/boo.mp3");
 
-  // Preload
-  bell.preload  = "auto";
-  cheer.preload = "auto";
-  boo.preload   = "auto";
+  [bell, cheer, boo].forEach(a => { a.preload = "auto"; });
+
+  // iOS requires audio to be "unlocked" via a direct user gesture.
+  // Play all sounds silently on first touch/click so they work in setTimeout later.
+  function unlock() {
+    [bell, cheer, boo].forEach(a => {
+      a.volume = 0;
+      a.play().then(() => { a.pause(); a.currentTime = 0; a.volume = 1; }).catch(() => {});
+    });
+  }
+  document.addEventListener("touchstart", unlock, { once: true });
+  document.addEventListener("click",      unlock, { once: true });
 
   function play(audio, maxSeconds) {
     audio.currentTime = 0;
@@ -18,7 +26,7 @@
   }
 
   window.SFX = {
-    bell:  () => play(bell, 2.0),
-    crowd: (positive) => play(positive ? cheer : boo, 2.0),
+    bell:  () => play(bell,  2.0),
+    crowd: (pos) => play(pos ? cheer : boo, 2.0),
   };
 })();
